6fffbab49d8151a871ad2481e6d57eee9fa56849
TeachMeHow <szymoncichy123@gmail.com>
2019-01-10 16:42:36 +0100

    send transfer request

--- src/bankingClient/TransferSender.java
+++ src/bankingClient/TransferSender.java
@@ -1,5 +1,7 @@
 package bankingClient;
 
+import java.io.IOException;
+
 public class TransferSender {
 	private Connection connection;
 	TransferSender(Connection connection)
@@ -8,10 +10,21 @@ public class TransferSender {
 	}
 	void execute(Transfer transfer)
 	{
-		String request;
-		
-		connection.sendRequest(request);
-		
+		String request = new String();
+		StringBuilder requestBuilder = new StringBuilder();
+		// server will know how to parse this request if it knows it's a transfer
+		requestBuilder.append("TRANSFER\n");
+		// transfer attributes to parse
+		requestBuilder.append("destination=" + transfer.getDestinationAccount() + '\n');
+		requestBuilder.append("source=" + transfer.getSourceAccount() + '\n');
+		requestBuilder.append("title=" + transfer.getTitle() + '\n');
+		requestBuilder.append("type=" + String.valueOf(transfer.getType()) + '\n');
+		requestBuilder.append("amount=" + transfer.getAmount().toString());
+		try {
+			connection.sendRequest(request);
+		} catch (IOException e) {
+			System.out.println("Request could not be sent");
+		}		
 	}
 	
 }
